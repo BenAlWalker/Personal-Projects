28aefff0d74b9c7df8feb29fad96a7035c2c05d0
BenAlWalker <98350971+BenAlWalker@users.noreply.github.com>
2022-07-02 01:25:17 -0400

    Include input line

--- heads_flips.py
+++ heads_flips.py
@@ -2,13 +2,14 @@ from random import random
 import numpy as np
 from matplotlib import pyplot as plt
 
+h_in_row = input("How many heads in a row to test for? ")
 counter = 0
 total_count = 0
 positions = []
 heads = 0
 total_flips = 0
 
-while counter < 20:
+while counter < int(h_in_row):
     prob = random()
     if 0 < prob < .5:  # flipped a heads
         counter += 1  # increase total heads by 1
@@ -19,15 +20,15 @@ while counter < 20:
         total_flips += 1
         positions.append(heads)
         heads = 0
-    if counter == 20:
+    if counter == int(h_in_row):
         positions.append(counter)
 
 print("There were " + str(total_flips) + " total flips.")
-print("Ther were " + str(len(positions)) + " total trials.")
+print("There were " + str(len(positions)) + " total trials.")
 
 x = np.arange(1, len(positions) + 1)
 plt.title("Quarter flips")
 plt.xlabel("Trials")
-plt.ylabel("Heads per Trial")
+plt.ylabel("Heads Per Trial")
 plt.plot(x, positions, 'k:')
 plt.show()
